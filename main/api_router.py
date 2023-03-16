2fb13034f10e3213572a53a029b760e74e6369c1
mrh0wl <h0wl.jacket@gmail.com>
2023-03-15 20:19:13 -0600

    Add new endpoints

--- main/api_router.py
+++ main/api_router.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter
 
-from api.endpoints import game_router, genre_router, platform_router, age_rating_router
+from api.endpoints import (game_router, genre_router, keyword_router,
+                           language_router, platform_router,
+                           player_perspective_router, theme_router)
 
 router = APIRouter()
 
 router.include_router(game_router, prefix="/games", tags=["Games"])
+router.include_router(player_perspective_router, prefix="/player-perspectives", tags=['Player Perspectives'])
 router.include_router(genre_router, prefix="/genres", tags=['Genres'])
+router.include_router(keyword_router, prefix="/keywords", tags=['Keywords'])
+router.include_router(theme_router, prefix="/themes", tags=['Themes'])
 router.include_router(platform_router, prefix="/platforms", tags=['Platforms'])
-router.include_router(age_rating_router, prefix="/age-ratings", tags=['Age Rating'])
+router.include_router(language_router, prefix="/languages", tags=['Languages'])
